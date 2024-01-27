41244c7233ae3df10c04150bf86b76ec33988d44
shub_o4 <90241581+shubo4@users.noreply.github.com>
2024-01-27 12:15:27 +0530

    Update custom.py

--- scripts/custom.py
+++ scripts/custom.py
@@ -1,5 +1,5 @@
 import os
 
-string = "python train.py --num_of_workers 1 --root /kaggle/input/extracting-attributes-from-fashion-images-jan-2024/train --backbone res50 --gpus 1 --dataset custom --config_file configs/custom.yaml --save_dir . "
+string = "python train.py --num_of_workers 1 --root /kaggle/input/extracting-attributes-from-fashion-images-jan-2024/train --backbone res50 --gpus 1 --dataset custom --config_file configs/custom.yaml --save_dir /kaggle/working "
 
 os.system(string)
