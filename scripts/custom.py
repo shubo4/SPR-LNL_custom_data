42f7d2345fc075972e443a3fc0347cf2cfb9f565
shub_o4 <90241581+shubo4@users.noreply.github.com>
2024-01-27 11:50:11 +0530

    Update custom.py

--- scripts/custom.py
+++ scripts/custom.py
@@ -1,15 +1,5 @@
 import os
 
-options = [
-    ('symmetric', 0.2),
-    ('symmetric', 0.4),
-    ('symmetric', 0.6),
-    ('asymmetric', 0.2),
-    ('asymmetric', 0.3),
-    ('asymmetric', 0.4)
-]
+string = "python train.py --backbone res50 --gpus 1 --dataset custom --config_file configs/custom.yaml --save_dir . "
 
-for noise_type, noise_rate in options:
-    string = "python train.py --backbone res50 --gpus 1 --dataset custom --config_file configs/custom.yaml --noise_type {} --noise_rate {} --save_dir {}".format(
-        noise_type, noise_rate, '{}-{}'.format(noise_type, noise_rate))
-    os.system(string)
+os.system(string)
