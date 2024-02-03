db37390d7d2632c61b5e32d0816839213d3e42c0
shub_o4 <90241581+shubo4@users.noreply.github.com>
2024-02-03 16:46:39 +0530

    Update train.py

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ if os.path.exists(args.save_dir) and args.overwrite:
     print('Existing log folder, move it to trashes!')
 writter = SummaryWriter(args.save_dir)
 
-data_loader = DatasetGenerator(dataframe = pd.read_csv(args.dataframe_path),
+data_loader = DatasetGenerator(dataframe = pd.read_csv(args.csv_path),
                                data_path= args.root,#os.path.join(args.root, args.dataset),
                                num_of_workers=args.num_workers,
                                seed=args.seed,
