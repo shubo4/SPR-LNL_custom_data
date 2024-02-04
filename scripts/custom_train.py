8577c319e845ba7421251fd372f581f7f4ebb448
shub_o4 <90241581+shubo4@users.noreply.github.com>
2024-02-04 15:45:08 +0530

    Update custom_train.py

--- scripts/custom_train.py
+++ scripts/custom_train.py
@@ -1,5 +1,5 @@
 import os
 
-string = "python train.py --config_file configs/custom.yaml --dataset custom"
+string = "python train.py --config_file configs/custom.yaml --dataset custom --tqdm 1 --root /kaggle/input/extracting-attributes-from-fashion-images-jan-2024/train --num_workers 4"
 
 os.system(string)
